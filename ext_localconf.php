6c9544482102a4dbbc3e5e78110db733d69bface
Oliver Eglseder <oliver.eglseder@in2code.de>
2020-03-06 16:31:21 +0100

    [REFACTOR] Use shorthand function invocation syntax

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,11 +3,9 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-call_user_func(
-    function () {
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:backend/Resources/Private/Language/locallang.xlf'][] = 'EXT:t3am/Resources/Private/Language/locallang.xlf';
-        $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['t3am_server'] = \In2code\T3AM\Server\Server::class . '::handle';
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['tx_t3am'] = \In2code\T3AM\Server\Hooks\ClientTokenGenerator::class;
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['tx_t3am_client_migration'] = \In2code\T3AM\Updates\ClientMigrationWizard::class;
-    }
-);
+(function () {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:backend/Resources/Private/Language/locallang.xlf'][] = 'EXT:t3am/Resources/Private/Language/locallang.xlf';
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['t3am_server'] = \In2code\T3AM\Server\Server::class . '::handle';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['tx_t3am'] = \In2code\T3AM\Server\Hooks\ClientTokenGenerator::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['tx_t3am_client_migration'] = \In2code\T3AM\Updates\ClientMigrationWizard::class;
+})();
