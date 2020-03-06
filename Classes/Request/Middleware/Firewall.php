77448daf61d5fcd7e1d2dd8cc64bc477c2090066
Oliver Eglseder <oliver.eglseder@in2code.de>
2020-03-06 16:22:03 +0100

    [REFACTOR] Move client token counting to ClientRepository

--- Classes/Request/Middleware/Firewall.php
+++ Classes/Request/Middleware/Firewall.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 namespace In2code\T3AM\Request\Middleware;
 
-use In2code\T3AM\Server\SecurityService;
+use In2code\T3AM\Domain\Repository\ClientRepository;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -12,19 +12,11 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class Firewall implements MiddlewareInterface
 {
-    /** @var SecurityService */
-    protected $tokenService = null;
-
-    public function __construct()
-    {
-        $this->tokenService = GeneralUtility::makeInstance(SecurityService::class);
-    }
-
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $token = $request->getQueryParams()['token'] ?? '';
 
-        if (!$this->tokenService->isValid($token)) {
+        if (GeneralUtility::makeInstance(ClientRepository::class)->countByToken($token) !== 1) {
             $response = new JsonResponse();
             $response->setPayload(['code' => 1519999361, 'error' => true, 'message' => 'Access error', 'data' => []]);
             return $response;
