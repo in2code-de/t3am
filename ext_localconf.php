fa8ad02441b8ff08e0c19e48b1905e47ff23e677
Ingo Fabbri <inf@mmc-agentur.at>
2020-04-22 15:10:06 +0200

    [TASK] register authentication service in ext_localconf.php
    see
    https://docs.typo3.org/m/typo3/reference-coreapi/master/en-us/ExtensionArchitecture/ConfigurationFiles/Index.html#should-be-used-for

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,25 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
 
-call_user_func(function () {
+defined('TYPO3_MODE') or die();
+
+(static function () {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:backend/Resources/Private/Language/locallang.xlf'][] = 'EXT:t3am/Resources/Private/Language/locallang.xlf';
-});
+
+    $config = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\In2code\T3AM\Client\Config::class);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
+        'dwo_connections',
+        'auth',
+        \In2code\T3AM\Client\Authenticator::class,
+        [
+            'title' => 'T3AM Client Authenticator',
+            'description' => 'Global authentication service',
+            'subtype' => 'getUserBE,authUserBE',
+            'available' => (int)$config->isValid(),
+            'priority' => 80,
+            'quality' => 80,
+            'os' => '',
+            'exec' => '',
+            'className' => \In2code\T3AM\Client\Authenticator::class,
+        ]
+    );
+})();
